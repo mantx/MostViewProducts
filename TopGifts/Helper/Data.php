ada32afda697a22d37064d2ec3228a91680d01af
Shawn <shawn.wang.uk@gmail.com>
2013-07-16 10:14:55 +0100

    refactorring directory structure to local/TF/TopGifts

---
+++ TopGifts/Helper/Data.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ * Magento Custom Module
+ * To display the top 100 most viewed gifts on a single, responsive page
+ *
+ * @category    TF
+ * @package     TF_TopGifts
+ * @copyright   
+ * @license     Free Distribution
+ */
+ 
+ /**
+ * Toproducts data helper
+ *
+ * @version    1.0
+ * @author     Shawn Wang (shawn.wang.uk@gmail.com)
+ */
+class TF_TopGifts_Helper_Data extends Mage_Core_Helper_Abstract
+{
+}  
+ 
+?>
---
+++ TopGifts/controllers/IndexController.php
@@ -0,0 +1,26 @@
+<?php
+
+/**
+ * Magento Custom Module
+ * To display the top 100 most viewed gifts on a single, responsive page
+ *
+ * @category    TF
+ * @package     TF_TopGifts
+ * @copyright   
+ * @license     Free Distribution
+ */
+ 
+ /**
+ * Index controller
+ *
+ * @version    1.0
+ * @author     Shawn Wang (shawn.wang.uk@gmail.com)
+ */
+class TF_TopGifts_IndexController extends Mage_Core_Controller_Front_Action {        
+    public function indexAction() {
+        echo 'Hello Index!';
+    }
+  
+}
+
+?>
